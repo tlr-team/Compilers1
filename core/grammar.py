cbba5c4a49d804e16da28a3485006ce82f8663cf
Leonel Garcia <l.garcia3@estudiantes.matcom.uh.cu>
2019-06-04 18:36:09 -0400

    Add AttributeProduction

--- core/grammar.py
+++ core/grammar.py
@@ -230,6 +230,35 @@ class Production(object):
     def IsEpsilon(self):
         return self.Right.IsEpsilon
 
+class AttributeProduction(Production):
+
+    def __init__(self, nonTerminal, sentence, attributes):
+        if not isinstance(sentence, Sentence) and isinstance(sentence, Symbol):
+            sentence = Sentence(sentence)
+        super(AttributeProduction, self).__init__(nonTerminal, sentence)
+
+        self.attributes = attributes
+
+    def __str__(self):
+        return '%s := %s' % (self.Left, self.Right)
+
+    def __repr__(self):
+        return '%s -> %s' % (self.Left, self.Right)
+
+    def __iter__(self):
+        yield self.Left
+        yield self.Right
+
+
+    @property
+    def IsEpsilon(self):
+        return self.Right.IsEpsilon
+
+    # sintetizar en ingles??????, pending aggrement
+    def syntetice(self):
+        pass
+
+
 class Grammar():
 
     def __init__(self):
