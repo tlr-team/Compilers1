8165ef575928a4681091f34768edcded58b74ad8
Leonel Garcia <l.garcia3@estudiantes.matcom.uh.cu>
2019-06-05 22:41:46 -0400

    Extreme grmmar

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-S --> sS| sdA|dsB
-A --> sd | epsilon
-B --> s
-B -->d | epsilon
+S --> 5s S| s d A|d s B 
+A -->s :d | epsilon 
+B --> ;s\
+B --> d. | epsilon
+C --> \d/ c
+D --> \a ;s d
+G --> \a s d
