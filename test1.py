57fa85edffededefb429b9c78e848ef747043fd3
Leonel Garcia <l.garcia3@estudiantes.matcom.uh.cu>
2019-06-22 21:06:28 -0400

    Modified test1

--- test1.py
+++ test1.py
@@ -1,39 +1,41 @@
-from core import *
+from parse_input import *
+from parsers import *
+from regex import *
 
-#G = parse_to_grammar(read_from_file("./test_regular.txt"))
-G = parse_to_grammar(read_from_file("./test_up.txt"))
+test_name = "reg1"
+G = parse_to_grammar(read_from_file(f"./examples/test_{test_name}.txt"))
 # print(G)
 # for i in G.Productions:
-# 	# if i.Left.Name in ('S', 'S_0', 'A', 'A_1', 'B', 'B_2'): 
+# 	# if i.Left.Name in ('S', 'S_0', 'A', 'A_1', 'B', 'B_2'):
 # 	print(i)
 # print('\n\n')
 
-#remove_left_rec(G)
-#lambda_productions(G)
+# remove_left_rec(G)
+# lambda_productions(G)
 # useless_productions(G)
 
-# print(G)
-# for i in G.Productions:
-#     # if i.Left.Name in ('S', 'S_0', 'A', 'A_1', 'B', 'B_2'): 
-# 	print(i)
-# print('\n\n')
 
-# useless_productions(G)
+print(is_regular_grammar(G))
+nfa = convert_to_nfa(G)
 
-# print(G)
-# for i in G.Productions:
-#     # if i.Left.Name in ('S', 'S_0', 'A', 'A_1', 'B', 'B_2'): 
-# 	print(i)
-#print(is_regular_grammar(G))
-#nfa = convert_to_nfa(G)
+print(nfa._repr_svg_())
+# f = open("./salida.html", "w+")
+# f.write(nfa._repr_svg_())
+# f.close()
 
-#f = open("./salida.html", "w+")
-#f.write(nfa._repr_svg_())
-#f.close()
+# print(nfa)
+# print(nfa.regexs)
+# print(regex_state_remove(nfa.regexs,1))
+# print(regex_from_nfa(nfa))
 
-#print(nfa)
-#print(nfa.regexs)
-#print(regex_state_remove(nfa.regexs,1))
-#print(regex_from_nfa(nfa))
+firsts= compute_firsts(G)
+follows = compute_follows(G, firsts)
 
-print(Remove_Unit_Productions(G))
+# print(remove_unit_productions(G))
+# print(remove_left_rec(G))
+ll1 = LL1(G,firsts=firsts,follows=follows)
+# lr1 = LR1(G)
+print(ll1)
+print(ll1("az")[1])
+# print(lr1)
+
